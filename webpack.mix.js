7579329ecaa6af55ef9e6e988462888e68ed4d38
Luis Angel <angel.rh.2108@gmail.com>
2023-03-09 16:58:34 -0600

    Correccion de errores

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,4 @@ const mix = require("laravel-mix");
 mix
     .styles(
         ["resources/css/app.css","resources/css/toast.css"],
-        "public/css/app.css")
-    .js(["resources/js/toast.js"],"public/js/app.js");
+        "public/css/app.css");
